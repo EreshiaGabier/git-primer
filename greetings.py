984ba8ac8568b1a41684b1964ecf98a15519006d
EreshiaGabier <ereshg@gmail.com>
2024-09-05 06:07:29 +0200

    Added colon to input

--- greetings.py
+++ greetings.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-name = input("Please state your name ")
+name = input("Please state your name: ")
 hour = datetime.now().time().hour
 greeting = "Good morning" if 4 <= hour < 16 else "Good night"
 print(f"{greeting}, {name}!")
