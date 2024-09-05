0a8e3358964a0677573ac52eb0957745157609e9
EreshiaGabier <ereshg@gmail.com>
2024-09-05 06:18:49 +0200

    added comment

--- greetings.py
+++ greetings.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+# added the comment to test
 name = input("Please state your name ")
 hour = datetime.now().time().hour
 greeting = "Good morning" if 4 <= hour < 16 else "Good night"
